8381d20f52fe00aad395d6d539b648b3175189f6
hereariim <herearii.metuarea@gmail.com>
2022-05-12 15:45:09 +0200

    yes

--- src/napari_seuillage/_widget.py
+++ src/napari_seuillage/_widget.py
@@ -9,6 +9,26 @@ Replace code below according to your needs.
 from magicgui import magic_factory
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
 
+from enum import Enum
+from subprocess import call
+import numpy
+import napari
+from napari.types import ImageData
+from magicgui import magic_factory, magicgui
+from skimage import data
+import sys
+
+import numpy
+
+import numpy as np
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.transform import resize
+from napari.qt.threading import thread_worker
+from napari.utils.notifications import show_info
+from skimage.io import imread
+from enum import Enum
+from napari.utils.notifications import show_info
+
 
 class ExampleQWidget(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
@@ -29,13 +49,31 @@ class ExampleQWidget(QWidget):
         print("napari has", len(self.viewer.layers), "layers")
 
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def do_otsu(layer: ImageData) -> ImageData:
+    
+    import cv2
+    img = cv2.cvtColor(layer, cv2.COLOR_BGR2GRAY)
+    ret, thresh1 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return thresh1
 
+def do_sobel(layer: ImageData) -> ImageData:
+    import cv2
+    img = cv2.cvtColor(layer, cv2.COLOR_BGR2GRAY)
+    gX = cv2.Sobel(img, cv2.CV_64F, 1, 0)
+    gY = cv2.Sobel(img, cv2.CV_64F, 0, 1)
+    # compute the gradient magnitude and orientation
+    magnitude = np.sqrt((gX ** 2) + (gY ** 2))
+    return magnitude
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+@magic_factory(call_button="Run",radio_option={"widget_type": "RadioButtons",
+                        "orientation": "vertical",
+                        "choices": [("Otsu",1), ("Sobel",2)]})
+def do_model_segmentation(
+    layer: ImageData, radio_option=1
+    ) -> ImageData:
+    if radio_option==1:
+        show_info('Succes !')
+        return do_otsu(layer)
+    if radio_option==2:
+        show_info('Succes !')
+        return do_sobel(layer)
